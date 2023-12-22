f05624a1ecb45fc8ae6871af4efe26d1e02900ef
Shravastee Thakur <123631680+shravastee-thakur@users.noreply.github.com>
2023-12-22 15:35:32 +0530

    Add files via upload

---
+++ Tic-Tac-Toe/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+import Board from './Component/Board'
+
+function App() {
+  
+  return (
+    <>
+      <Board />
+      
+    </>
+  )
+}
+
+export default App

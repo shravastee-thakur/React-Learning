30893a63e400d1ae32795eb832e7d1c298f4a1ce
Shravastee Thakur <123631680+shravastee-thakur@users.noreply.github.com>
2024-05-12 14:15:57 +0530

    Add files via upload

---
+++ Cart with filter/src/App.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import './App.css'
+import { useState } from "react";
+import Header from "./Components/Header";
+import Products from "./Components/Products";
+
+
+
+function App() {
+
+
+
+
+  return (
+    <>
+      <Header />
+      <Products />
+    </>
+  )
+}
+
+export default App;
---
+++ Cart with filter/src/Components/Header.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "./Header.css";
+
+const Header = () => {
+  return (
+    <div className="navbar">
+      <div className="nav-item">
+        <p>Home</p>
+      </div>
+      <div className="nav-item">
+        <p>Cart</p>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
